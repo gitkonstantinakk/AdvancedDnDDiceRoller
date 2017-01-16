dd976cc1eef7bae5b7dfe1a866ecc690d6b81d65
lazarlyutakov <lazarlyutakov@gmail.com>
2017-01-17 00:09:27 +0200

    enums extracted

---
+++ AdvDiceRoller/Console/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvDiceRoller.Console
+{
+    public enum GameState
+    {
+        gameOn,
+        gameOff
+    }
+}
